5aa478852c7838f0c927cde315871ddaa4aed70c
SathishSegu <sathishsegu123@gmail.com>
2026-02-10 10:38:24 +0530

    Initial Spring boot project setup

---
+++ src/main/java/com/course/courseenrollmentapi/CourseEnrollmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.course.courseenrollmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    CourseEnrollmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEnrollmentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/courseenrollmentapi/CourseEnrollmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.courseenrollmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
